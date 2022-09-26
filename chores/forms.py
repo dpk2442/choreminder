4c5f2073059409666d539f7217b78a2a301c8dde
Dave Korhumel <dpk2442@gmail.com>
2022-09-26 01:05:21 -0700

    improve the formatting of the chore form

--- chores/forms.py
+++ chores/forms.py
@@ -7,3 +7,7 @@ class ChoreForm(forms.ModelForm):
     class Meta:
         model = models.Chore
         fields = ["name", "description", "due_duration", "overdue_duration"]
+        help_texts = {
+            "due_duration": "The amount of time since last completion before a task is considered due.",
+            "overdue_duration": "The amount of time after a task is due before a it is considered overdue.",
+        }
