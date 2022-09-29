52a0ccc160f9d142de7a29cea32ec2314a0326aa
Dave Korhumel <dpk2442@gmail.com>
2022-09-28 18:12:05 -0700

    add model view base class

--- chores/model_views.py
+++ chores/model_views.py
@@ -1,31 +1,48 @@
+from typing import Generic, TypeVar
+
+from django.db.models import Model as DjangoModel
 from django.utils import timezone
 
 from chores import models
 
+DjangoModelType = TypeVar("DjangoModelType", bound=DjangoModel)
 
-class Chore(object):
+
+class ModelViewBase(Generic[DjangoModelType]):
+
+    def __init__(self, obj: DjangoModelType):
+        self._obj = obj
+
+    def __str__(self) -> str:
+        return str(self._obj)
+
+    def __eq__(self, other: object) -> bool:
+        return isinstance(other, ModelViewBase) and self._obj == other._obj
+
+
+class Chore(ModelViewBase):
 
     def __init__(self, chore: models.Chore):
-        self._chore = chore
+        super().__init__(chore)
         self._fetched_latest_log = False
         self._latest_log = None
 
     @property
     def id(self):
-        return self._chore.id
+        return self._obj.id
 
     @property
     def name(self):
-        return self._chore.name
+        return self._obj.name
 
     @property
     def description(self):
-        return self._chore.description
+        return self._obj.description
 
     @property
     def latest_log(self):
         if not self._fetched_latest_log:
-            db_log = self._chore.log_set.order_by("-timestamp").first()
+            db_log = self._obj.log_set.order_by("-timestamp").first()
             if db_log is not None:
                 self._latest_log = Log(db_log)
 
@@ -35,7 +52,7 @@ class Chore(object):
         if self.latest_log is None:
             return None
 
-        return self.latest_log.timestamp + self._chore.due_duration
+        return self.latest_log.timestamp + self._obj.due_duration
 
     def display_status(self):
         now = timezone.now()
@@ -43,7 +60,7 @@ class Chore(object):
         if next_due is None:
             return "N/A"
 
-        overdue = next_due + self._chore.overdue_duration
+        overdue = next_due + self._obj.overdue_duration
         if next_due > now:
             return "Completed"
         elif now < overdue:
@@ -51,24 +68,12 @@ class Chore(object):
         else:
             return "Overdue"
 
-    def __str__(self) -> str:
-        return str(self._chore)
 
-    def __eq__(self, other) -> bool:
-        return isinstance(other, Chore) and self._chore == other._chore
-
-
-class Log(object):
+class Log(ModelViewBase):
 
     def __init__(self, log: models.Log):
-        self._log = log
+        super().__init__(log)
 
     @property
     def timestamp(self):
-        return self._log.timestamp
-
-    def __str__(self) -> str:
-        return str(self._log)
-
-    def __eq__(self, other) -> bool:
-        return isinstance(other, Log) and self._log == other._log
+        return self._obj.timestamp
